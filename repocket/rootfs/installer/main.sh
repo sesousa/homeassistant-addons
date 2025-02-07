b983d45e4429b028a9d8f0765e72987c0f0eb2aa
root <root@core-ssh.local.hass.io>
2025-02-07 20:37:40 +0000

    Added updated config.

--- repocket/rootfs/installer/main.sh
+++ repocket/rootfs/installer/main.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 ## Repocket files
 mkdir -p /app/
 tar -zxf /repo/repocket.tar.gz -C /app/
 
 ## Dependencies install
-cd /app
-npm install
+# cd /app
+# npm install
